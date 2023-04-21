85fa36417cce690c3651de18d1ea94b60df95aee
LordMewtwo73 <65983752+LordMewtwo73@users.noreply.github.com>
2023-04-21 08:16:43 -0400

    Add files via upload

---
+++ assets/ModFiles/compile.bat
@@ -0,0 +1,1 @@
+for /r %%x in (*.xml) do "D:\FE_Engage\Engage_Randomizer\Engage_Randomizer\bin\Debug\assets\EngageXml\EngageXml.exe" -in %%x "%%x.bundle"
---
+++ assets/ModFiles/export.bat
@@ -0,0 +1,1 @@
+for /r %%x in (*.xml.bundle) do "D:\FE_Engage\Engage_Randomizer\Engage_Randomizer\bin\Debug\assets\EngageXml\EngageXml.exe" -out %%x
